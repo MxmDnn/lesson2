349baa5e9de1036ad24c145634279dfb6ec5e53c
MxmDnn <mxmjuk@gmail.com>
2024-07-11 22:37:34 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework_exercises = 12
+time_of_homework_exercises = 1.5
+name_of_course = 'Python'
+time_of_exercise = (time_of_homework_exercises/homework_exercises)
+print('Курс: ' + name_of_course + ',','всего задач:',homework_exercises,'затрачено часов:', time_of_homework_exercises,'среднее время выполнения', time_of_exercise)
